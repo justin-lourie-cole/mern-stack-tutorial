e1d5e1a040ffeb82af89e505b4a2a4b6e9682f17
Justin Cole <justincole@outlook.co.nz>
2020-12-05 23:51:47 +1300

    (feat) tutorial complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Navbar from "./components/navbar.component";
+import ExercisesList from "./components/exercises-list.component";
+import EditExercise from "./components/edit-exercise.component.js";
+import CreateExercise from "./components/create-exercise.component.js";
+import CreateUser from "./components/create-user.component.js";
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="container">
+			<Router>
+				<Navbar />
+				<br />
+				<Route path="/" exact component={ExercisesList} />
+				<Route path="/edit/:id" component={EditExercise} />
+				<Route path="/create" component={CreateExercise} />
+				<Route path="/user" component={CreateUser} />
+			</Router>
+		</div>
+	);
 }
 
 export default App;
